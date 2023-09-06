e03e8492180be3b087ef934170d4c4961187c9ce
redhun <redhuns123@gmail.com>
2023-09-06 22:05:30 +0530

    grid layout added

---
+++ lib/screens/categories.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/data/dummy_data.dart';
+import 'package:mealsapp/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick Your Category'),
+      ),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(category: category),
+          ]),
+    );
+  }
+}
